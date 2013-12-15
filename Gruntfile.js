be7fba6c12faa0fbc10f7b152ae279750de16def
widmogrod <widmogrod@gmail.com>
2013-12-15 12:36:45 +0100

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            all: ['Gruntfile.js', 'js/**/*.js']
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'js/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            scripts: {
+                files: ['js/**/*.js'],
+                tasks: ['jshint'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    name: 'game',
+                    baseUrl: "js",
+                    mainConfigFile: "js/config.js",
+                    out: "js/optimized.js"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
