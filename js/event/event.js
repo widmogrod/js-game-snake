d04c9838f9ceb945d319be85c5eb536c8e29e659
widmogrod <widmogrod@gmail.com>
2013-12-21 19:37:15 +0100

    add to event result collection

--- js/event/event.js
+++ js/event/event.js
@@ -1,5 +1,36 @@
 define(function(){
 
+    function Result(event) {
+        this.event = event;
+        this.results = [];
+    }
+    Result.prototype.count = function() {
+        return this.results.length;
+    }
+    Result.prototype.push = function(value) {
+        this.results.push(value);
+    }
+    Result.prototype.last = function(defaults) {
+        var c = this.count();
+        return c ? this.results[c - 1] : defaults;
+    }
+    Result.prototype.each = function(func) {
+        this.results.forEach(func);
+    }
+
+    function createEvent() {
+        var stopPropagation = false;
+        return {
+            stopPropagation: function(flag) {
+                if (arguments.length) {
+                    stopPropagation = !!flag;
+                } else {
+                    return stopPropagation;
+                }
+            }
+        }
+    }
+
     function Event() {
         this.events = {};
     }
@@ -9,12 +40,24 @@ define(function(){
         return this;
     }
     Event.prototype.trigger = function(name, args) {
+        var value, events;
+        var event = typeof this.createEvent === 'function' ? this.createEvent() : createEvent(),
+            result = new Result(event);
+
         if (!this.events.hasOwnProperty(name)) {
-            return;
+            return result;
         }
-        this.events[name].forEach(function(callback) {
-            callback.apply(null, args);
-        })
+
+        args = [event].concat(args);
+        events = this.events[name];
+
+        for (var i = 0, length = events.length; i < length; i++) {
+            value = events[i].apply(null, args);
+            result.push(value);
+            if (event.stopPropagation()) break;
+        }
+
+        return result;
     }
     Event.prototype.proxy = function(name, args) {
         var self = this;
