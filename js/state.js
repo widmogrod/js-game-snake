300ba7e27d6d8c582f7ce0fafed269b601dfd39c
widmogrod <widmogrod@gmail.com>
2013-12-21 19:40:11 +0100

    now StateManager has possibility to look state untll unlock

--- js/state.js
+++ js/state.js
@@ -1,13 +1,34 @@
-define(['event/event'], function(Event){
+define(['event/event', 'promise'], function(Event, Promise){
+    function onAny(context) {
+        return function() {
+            --context.lock;
+        }
+    }
+
     function onChange(from, to, context) {
         return function() {
+            var results;
             if (null !== context.state && (context.state !== from && from !== '*')) {
+                // console.log('NO - state');
                 return;
             }
 
+            if (context.unlock && !context.unlock()) {
+                context.postponed = onChange(from, to, context);
+                // console.log('NO - unlock');
+                return;
+            }
+
+
+            // console.log('OK - unlocked')
+
+            context.unlock = null;
             context.state = to;
             context.trigger('change', [from, to]);
-            context.trigger('enter:' + to, [from]);
+            results = context.trigger('enter:' + to, [from]);
+            if (results.event.hasLocks()) {
+                context.unlock = results.event.unlock;
+            }
         }
     }
 
@@ -23,6 +44,7 @@ define(['event/event'], function(Event){
         var self = this;
         self.events = {};
         self.state = null;
+        self.unlock = null;
 
         each(states, function(events, state) {
             each(events, function(nextState, event){
@@ -32,6 +54,43 @@ define(['event/event'], function(Event){
     }
 
     StateMachine.prototype = new Event();
+    StateMachine.prototype.run = function() {
+        // Resove postponed state.
+        // This can happen when current state is bloked
+        if (typeof this.postponed === 'function') {
+            var func = this.postponed;
+            this.postponed = null;
+            func();
+        }
+    }
+    StateMachine.prototype.createEvent = function() {
+        var stopPropagation = false;
+        var unlocks = [];
+        return {
+            hasLocks: function() {
+                return unlocks.length > 0;
+            },
+            lock: function(func) {
+                unlocks.push(func);
+            },
+            unlock: function() {
+                var result = true;
+                unlocks.forEach(function(func){
+                    if (result) {
+                        result = func();
+                    }
+                });
+                return result;
+            },
+            stopPropagation: function(flag) {
+                if (arguments.length) {
+                    stopPropagation = !!flag;
+                } else {
+                    return stopPropagation;
+                }
+            }
+        }
+    }
 
     return StateMachine;
 })
