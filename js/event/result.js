342342ade922ce6e94c5d728c8848a633c1427e3
widmogrod <widmogrod@gmail.com>
2013-12-21 19:39:07 +0100

    event result is now in new place

---
+++ js/event/result.js
@@ -0,0 +1,19 @@
+define(function(){
+    function Result(event) {
+        this.event = event;
+        this.results = [];
+    }
+    Result.prototype.count = function() {
+        return this.results.length;
+    }
+    Result.prototype.push = function(value) {
+        this.results.push(value);
+    }
+    Result.prototype.last = function(defaults) {
+        var c = this.count();
+        return c ? this.results[c - 1] : defaults;
+    }
+    Result.prototype.each = function(func) {
+        this.results.forEach(func);
+    }
+})
