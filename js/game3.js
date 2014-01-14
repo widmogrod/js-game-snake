0d5f5f4dc2b3f09c1856c8e382013a28dd6c3a74
widmogrod <widmogrod@gmail.com>
2014-01-14 11:51:32 +0100

    game3 has blender objects

--- js/game3.js
+++ js/game3.js
@@ -5,7 +5,6 @@ define([
     'math/matrix4',
     'math/vector3',
     'math/eangle',
-    'shape/mesh/cube',
     'shape/color',
     'game/config',
     'shape/utils/assets'
@@ -17,7 +16,6 @@ function(
     Matrix4,
     Vector3,
     EAngle,
-    CubeMesh,
     Color,
     GameConfig,
     AssetUtil
@@ -26,8 +24,8 @@ function(
 
     function SomeGame(canvas) {
         this.assets = new AssetUtil(GameConfig.BASE_URL);
-        this.assets.loadBlenderMesh('cube', 'cube.dae');
         this.assets.loadBlenderMesh('monkey', 'monkey.dae');
+        this.assets.loadBlenderMesh('cube', 'cube.dae');
 
         this.renderer = new Renderer(canvas);
 
@@ -36,7 +34,7 @@ function(
             viewportMain,
             this.renderer,
             Matrix4.lookAtLH(
-                new Vector3(0, 0, 200),
+                new Vector3(0, 0, 700),
                 Vector3.zero(),
                 Vector3.up()
             ),
@@ -45,42 +43,33 @@ function(
 
         document.addEventListener("keydown", this.captureKeys.bind(this), false);
 
-        this.cube = new CubeMesh(0, 0, GameConfig.BOARD_EDGE, GameConfig.CUBE_FIELD_SIZE, Color.fromName('red'));
-
         this.meshes = []
-        this.meshes.push(this.cube);
-
-        var mesh = new CubeMesh(0, 0, 0, GameConfig.BOARD_WIDTH, Color.fromName('green'));
-        // this.meshes.push(mesh);
+        this.rotation = new Vector3(0, 0, 0);
 
         var self = this;
-        this.assets.get('cube', function(mesh) {
-            console.log('cube', mesh);
-            // self.meshes.push(mesh)
-        })
         this.assets.get('monkey', function(mesh) {
-            // console.log('monkey', mesh);
-            mesh.scale.x = 100;
-            mesh.scale.y = 100;
-            mesh.scale.z = 100;
+            mesh.scale.x = 10;
+            mesh.scale.y = 10;
+            mesh.scale.z = 10;
+            self.meshes.push(mesh)
+        })
+        this.assets.get('cube', function(mesh) {
+            mesh.translation.x = 20;
             self.meshes.push(mesh)
         })
     }
 
     SomeGame.prototype.captureKeys = function(e) {
-        var position = EAngle.fromVector(this.cube.rotation);
+        var position = EAngle.fromVector(this.rotation);
 
         switch(e.keyCode) {
-            case 37: e.preventDefault(); this.cube.rotation.y -= 10; break; // left
-            case 39: e.preventDefault(); this.cube.rotation.y += 10; break; // right
-            case 38: e.preventDefault(); this.cube.translation = this.cube.translation.add(position.toVector().scale(10)); break; // up
-            case 40: e.preventDefault(); this.cube.translation = this.cube.translation.subtract(position.toVector().scale(10)); break; // down
-            default: console.log(e.keyCode);
+            case 37: e.preventDefault(); this.rotation.y -= 10; break; // left
+            case 39: e.preventDefault(); this.rotation.y += 10; break; // right
         }
 
-        // Bind camera orientation to the cube
-        var eye = this.cube.translation.subtract(position.toVector().scale(400));
-        var at = this.cube.translation.add(position.toVector());
+        // Bind camera orientation to the player
+        var eye = position.toVector().scale(30);
+        var at = position.toVector();
         this.engine.viewMatrix = Matrix4.lookAtLH(eye, at, Vector3.up());
 
         this.run();
@@ -89,7 +78,6 @@ function(
         this.renderer.clean();
         this.engine.render(this.meshes);
         this.renderer.render();
-        // requestAnimationFrame(this.run.bind(this));
     }
 
     return SomeGame;
--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ require.config({
     ,optimize: "none"
 });
 
-require(['game3'], function(TetrisGame) {
-    "use strict";
+require(['game4'], function(TetrisGame) {
+    'use strict';
 
     var tetris, game;
 
@@ -24,18 +24,4 @@ require(['game3'], function(TetrisGame) {
 
     tetris = new TetrisGame(game);
     tetris.run();
-
-    // require(['math/matrix'], function(Matrix) {
-    //     var identity = Matrix.identity(3);
-    //     console.log(identity.toString());
-    //     var a = new Matrix(2, [1, 0, 2, -1, 3, 1]);
-    //     console.log('a', a.toString());
-    //     console.log('scalar', a.scalar(-1).toString());
-    //     console.log('transposed', a.transpose().toString());
-    //     var b = new Matrix(3, [3, 1, 2, 1, 1, 0]);
-    //     console.log(b.toString());
-    //     var r = a.multiply(b);
-    //     console.log(r.toString());
-    // });
-
 });
