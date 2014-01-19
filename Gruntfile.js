bba02ca2b8a36ae7ff60561440c72dc9d27e28d1
widmogrod <widmogrod@gmail.com>
2014-01-19 11:33:09 +0100

    force branche checkout

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function(grunt) {
                     'rm -rf build',
                     'git commit -am "new realise"',
                     'git push origin gh-pages',
-                    'git checkout master'
+                    'git checkout master -f'
                 ].join(' && ')
             }
         },
