e840c3306b8cd86d23cbf4509d06f6aaec4bba20
widmogrod <widmogrod@gmail.com>
2014-01-19 17:55:29 +0100

    reduce animation frame

--- js/game6.js
+++ js/game6.js
@@ -177,7 +177,9 @@ function(
         this.bottomRight.render(this.meshes);
         this.collision.run();
         this.renderer.render();
-        requestAnimationFrame(this.run.bind(this))
+        // requestAnimationFrame(this.run.bind(this))
+
+        setTimeout(this.run.bind(this), 500);
     }
 
     return SomeGame;
